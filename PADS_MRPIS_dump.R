385c189b3c39a4fb1a7fef0aee03f8b3c872e722
Katie Davidson <katie.davidson92@gmail.com>
2023-05-24 13:57:58 -0700

    Update PADS_MRPIS_dump.R
    cleaned

--- PADS_MRPIS_dump.R
+++ PADS_MRPIS_dump.R
@@ -4,13 +4,15 @@
 # May 2023
 
 
-
-# ========================= SETUP =========================
+# ========================= SET UP =========================
 
 # Load packages ---------------------------
+library(tidyverse)
 library(httr)          # for authenticate()
 library(askpass)       # for askpass()
 
+# Set sample year of interest ---------------------------
+analysis_year <- 2022
 
 # Compile credentials into http authentication function to access online DFO databases ---------------------------
 auth <- authenticate(user = Sys.info()["login"], 
@@ -19,10 +21,13 @@ auth <- authenticate(user = Sys.info()["login"],
 
 
 
+
 # ========================= DEFINE EXTRACTION FUNCTIONS =========================
 
-# Function for getting container metadata (gives 'CtnStartDate' field) ---------------------------
+# Multiple functions are needed to access different parts of the PADS database right now. They produce different dataframes that are eventually joined below. 
 
+
+# Function for getting container metadata (gives 'CtnStartDate' field) ---------------------------
 get_container_meta <- function(batch_id){
   url <- paste0('http://pac-salmon.dfo-mpo.gc.ca/Api.CwtDataEntry.v2/odata/GetBatchContainers')
   request_body <- paste0(r"({"batch_id":")", batch_id, r"("})")
@@ -38,7 +43,6 @@ get_container_meta <- function(batch_id){
 
 
 # Function for getting batch metadata (gives 'Location' field) ---------------------------
-
 get_batch_meta <- function(batch_id){
   url <- paste0('http://pac-salmon.dfo-mpo.gc.ca/Api.CwtDataEntry.v2/api/AgeBatchDetail/GetAgeBatchDetail/', batch_id)
   x <- httr::GET(url, auth)
@@ -59,8 +63,8 @@ get_batch_meta <- function(batch_id){
   return(y)
 }
 
-# Function for extracting batch age results ---------------------------
 
+# Function for extracting batch age results ---------------------------
 get_age_data <- function(batch_id){
   url <- paste0('http://pac-salmon.dfo-mpo.gc.ca/Api.CwtDataEntry.v2/api/AgeBatchDetail/ScaleAgeDetail/', 
                 batch_id)
@@ -73,68 +77,64 @@ get_age_data <- function(batch_id){
 
 
 
+# ========================= EXTRACT DATA =========================
 
-
-
-
-
-
-
-
-# Extract all South Coast scale batch IDs for SampleYear of interest and store in a vector of IDs called "batch_list_2022" ---------------------------
+# Extract all South Coast scale batch IDs for SampleYear of interest and store in a vector of IDs called "PADS_batch_ids_2022" ---------------------------
 PADS_batch_ids_2022 <- httr::GET('http://pac-salmon.dfo-mpo.gc.ca/Api.CwtDataEntry.v2/api/Report/GetAgeBatchList',
                                  auth) %>%
-  # Get list of available age result batches:
+   # Get list of available age result batches:
   httr::content(x = .,  'text') %>%
   jsonlite::fromJSON() %>%
-  # Filter batch list to keep only South Coast samples from 2022:
+    # Filter batch list to keep only South Coast samples from 2022:
   filter(Sector == "SC",
          SampleYear == analysis_year) %>%
-  pull(Id) # Extract column of age batch Ids from resulting dataframe
+  pull(Id)                                                       # Extract column of age batch Ids from resulting dataframe
 
 
 
-# Extract batch metadata, container metadata, and age data for scales in batch_list_2022; Join into 1 dataframe ---------------------------
-### intermediate tables are ignored for now, but can be run to help with diagnosing code issues if needed
+# Extract batch metadata, container metadata, and age data for scales in PADS_batch_ids_2022; Join into 1 dataframe ---------------------------
+#   intermediate tables names are ignored for now, but can be run to help with diagnosing code issues if needed
+
 allPADS_2022 <- 
   full_join(
     # Batch metadata ---
-    #PADS_batch_meta <- 
+      # PADS_batch_meta <- 
     PADS_batch_ids_2022 %>%
       purrr::map_dfr(get_batch_meta) %>%
       rename(BatchId = Id), 
     # Container metadata ---
-    #PADS_container_meta <- 
+      # PADS_container_meta <- 
     PADS_batch_ids_2022 %>%
       purrr::map_dfr(get_container_meta) %>%
       rename(ContainerId = Id), 
     by="BatchId") %>%
   full_join(.,
             # Age data ---
-            #PADS_age_data <- 
+              # PADS_age_data <- 
             PADS_batch_ids_2022 %>%
               purrr::map_dfr(get_age_data) %>%
               rename(BatchId = Id),
-            by=c("BatchId", "ProjectName", "Region", "Area", "Species", "RecoveryYear", "LifeHistory", "ContainerId")) %>%   # do not join by all fields as there are entry inconsistencies, e.g., "ProjectPriority==H vs ==High")
+            by=c("BatchId", "ProjectName", "Region", "Area", "Species", "RecoveryYear", "LifeHistory", "ContainerId")) %>%          # DO NOT join by all fields as there are entry inconsistencies, e.g., "ProjectPriority==H vs ==High")
   mutate(`(R) SCALE BOOK NUM` = ContainerId,
          `(R) SCALE CELL NUM` = FishNumber,
          `(R) SCALE BOOK-CELL CONCAT` = case_when(!is.na(ContainerId) & !is.na(FishNumber) ~ paste0(ContainerId,sep="-",FishNumber))) %>%
-  select(-c(AgingStructure)) %>%  #remove duplicate column 
+  select(-c(AgingStructure)) %>%                       # remove duplicate column 
   print()
 
 
-# Data QC to make sure PADS join worked ---------------------------
+# Quick QC to make sure PADS join worked ---------------------------
 nrow(PADS_batch_ids_2022 %>%
        purrr::map_dfr(get_age_data) %>%
        rename(BatchId = Id))
 nrow(allPADS_2022)
-# There will likely be a difference in the number of data rows in the Join vs. the number that have been aged - this is likely because the lab hasn't
-# completed all aging yet. 
+# There will likely be a difference in the number of data rows in the Join vs. the number that have been aged - this is likely because the lab hasn't completed all aging yet. 
 
-# Do the math to confirm that the unaged samples account for the difference (roughly), should result in TRUE: 
+
+# Do the math to confirm that the unaged samples account for the difference (roughly); should result in TRUE: 
 # (total # samples in PADS join) - (# unaged samples in the PADS join) == (total # samples available) 
 nrow(allPADS_2022) - allPADS_2022%>%filter(is.na(GrAge)&is.na(ScaleCondition))%>%summarize(n=n())%>%pull(n) == nrow(PADS_batch_ids_2022 %>%
                                                                                                                       purrr::map_dfr(get_age_data) %>%
                                                                                                                       rename(BatchId = Id))
-# note that there are sometimes a handful of issues, e.g., coho in a sockeye test fishery that won't always match up properly 
+                                                                                                                      
+# NOTE: there are sometimes a handful of issues, e.g., coho in a sockeye test fishery that won't always match up properly 
 
